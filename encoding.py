a5d3ea414cdd4aa5ef9b236c4b331cf4165cc4fb
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-04 21:18:41 +0430

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -4,5 +4,12 @@ l=len(a)
 for i in range(0,l):
     c=ord(a[i])
     c=c+3
+    if(a[i].isupper()==True):
+        if(c==91):
+            c=65
+        elif(c==92):
+            c=66
+        elif(c==93):
+            c=67
     b.append(chr(c))
 print(*b,sep='')
