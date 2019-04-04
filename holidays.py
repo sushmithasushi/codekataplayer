bed0fc90346adddd947c2738782d9c7d68a94deb
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-04 20:29:30 +0430

    Create holidays.py

---
+++ holidays.py
@@ -0,0 +1,7 @@
+a=str(input())
+if(a=="Saturday")or(a=="Sunday"):
+	print("yes")
+elif(a=="Monday")or(a=="Tuesday")or(a=="Wednesday")or(a=="Thursday")or(a=="Friday"):
+	print("no")
+else:
+	print("invalid")
