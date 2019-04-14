4432304d6edab21e2dfee55e5e23c3f6fe6b5621
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-14 18:30:31 +0430

    Create set41.py

---
+++ set41.py
@@ -0,0 +1,25 @@
+class s:
+    def __init__(self):
+        self.items=[]
+    def push(self,data):
+        if(data==')'):
+            self.pop()
+        else:
+            self.items.append(data)
+    def pop(self):
+        self.items.pop()
+    def is_empty(self):
+        return self.items==[]
+o=s()
+l=str(input())
+
+
+for i in range(0,len(l),1):
+    o.push(l[i])
+
+v=0
+v=o.is_empty()
+if(v==1):
+    print("Yes")
+else:
+    print("No")
